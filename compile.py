afd24a0470a46ae0c9a3e5df97aac713f99b2b35
ChristianA <christiand.apostol@outlook.com>
2022-12-03 16:43:51 -0500

    divided assembly code into instructions & data

--- compile.py
+++ compile.py
@@ -1,15 +1,26 @@
 def read_assembly():
     """Reads each line as an individual instruction from assembly.txt and returns them in an array"""
     with open('assembly.txt') as f:
-        instructions = f.readlines()
-    print(instructions)
+        assembly = [line.rstrip() for line in f] # Strip '\n' from each line
+
+    assembly.remove("") # Trim all empty lines from the instruction
+    for i, line in enumerate(assembly):
+        if line == "DATA": # Identify the data break
+            # Use list indexing to divide
+            instructions = assembly[0:i]
+            data = assembly[i+1:len(assembly)]
+
+    # index 0 holds all the assembly instructions, index 1 holds all the data assignments
+    return [instructions, data]
 
 
 def compile_ins_img(instructions):
     """Takes an array of instructions and outputs image file (one for instruction_mem and one for data_mem)"""
 
 def compile_data_img(data):
-    
+    """Takes an array of memory assignments and outputs image file"""
 
 if __name__ == "__main__":
-    read_assembly()
+    program = read_assembly()
+    #compile_ins_img(program[0])
+    #compile_data_img(program[1])
