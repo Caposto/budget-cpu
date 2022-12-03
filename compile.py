8d2e20b926d5bab5b9ad0f04b48c6468460bf966
ChristianA <christiand.apostol@outlook.com>
2022-12-03 15:21:26 -0500

    read instructions from assembly

--- compile.py
+++ compile.py
@@ -1,10 +1,15 @@
-
 def read_assembly():
     """Reads each line as an individual instruction from assembly.txt and returns them in an array"""
+    with open('assembly.txt') as f:
+        instructions = f.readlines()
+    print(instructions)
 
 
-def compile_asm(instructions):
+def compile_ins_img(instructions):
     """Takes an array of instructions and outputs image file (one for instruction_mem and one for data_mem)"""
 
+def compile_data_img(data):
+    
+
 if __name__ == "__main__":
-    print("Compile")
+    read_assembly()
