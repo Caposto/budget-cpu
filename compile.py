2d3a69ab403be52a98d3fbc8900a8fa604bbe505
ChristianA <christiand.apostol@outlook.com>
2022-12-03 13:45:32 -0500

    first commit

---
+++ compile.py
@@ -0,0 +1,10 @@
+
+def read_assembly():
+    """Reads each line as an individual instruction from assembly.txt and returns them in an array"""
+
+
+def compile_asm(instructions):
+    """Takes an array of instructions and outputs image file (one for instruction_mem and one for data_mem)"""
+
+if __name__ == "__main__":
+    print("Compile")
